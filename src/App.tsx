610f7518201b7585a24dbef2e093cb783d79e15c
Mauricio Erazo <antonierazo13@hotmail.com>
2024-12-13 16:10:16 +0100

    feat: added useContext to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { useContext } from 'react';
 import { BudgetForm } from './components/BudgetForm';
 import { Header } from './components/Header';
+import { BudgetContext } from './context/BudgetContext';
 
 const App = () => {
+  const context = useContext(BudgetContext);
+  console.log(context);
+  
   return (
     <div>
       <Header />
