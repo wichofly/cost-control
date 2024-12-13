87f217b48dc84fba3dfb16d5933c75dbab8fa4bc
Mauricio Erazo <antonierazo13@hotmail.com>
2024-12-13 15:51:38 +0100

    feat: contextAPI created

---
+++ src/context/BudgetContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, Dispatch, ReactNode, useReducer } from 'react';
+import {
+  BudgetActions,
+  budgetReducer,
+  BudgetState,
+  initialState,
+} from '../reducers/budgetReducer';
+
+interface BudgetContextProps {
+  state: BudgetState;
+  dispatch: Dispatch<BudgetActions>;
+}
+
+interface BudgetProviderProps {
+  children: ReactNode;
+}
+
+export const BudgetContext = createContext<BudgetContextProps>(null!);
+
+export const BudgetProvider = ({ children }: BudgetProviderProps) => {
+  const [state, dispatch] = useReducer(budgetReducer, initialState);
+
+  return (
+    <BudgetContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      {children}
+    </BudgetContext.Provider>
+  );
+};
