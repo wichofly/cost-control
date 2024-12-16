b683e29fb064a179e99bdebbf32b2a0509622e89
Mauricio Erazo <antonierazo13@hotmail.com>
2024-12-16 22:34:08 +0100

    feat: create component to display amounts and graphs.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import expensePlannerImage from '../assets/expense-planner.webp';
 
 export const Header = () => {
   return (
-    <header className="bg-sky-600 py-6 max-h-72">
+    <header className="bg-sky-600 py-6  max-h-72">
       <div className="flex justify-around items-center">
         <h1 className="uppercase font-bold text-4xl text-white">
           Expense planner
