b49791c9918e06d26b6c3db98e163c8e502e031e
Mauricio Erazo <antonierazo13@hotmail.com>
2024-12-16 21:52:18 +0100

    feat: Conditionally displayed a component if it is budget is valid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useMemo } from 'react';
 import { BudgetForm } from './components/BudgetForm';
 import { Header } from './components/Header';
+import { useBudget } from './hooks/useBudget';
+import { BudgetTracker } from './components/BudgetTracker';
 
 const App = () => {
+  const { state } = useBudget();
+
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget]);
+
   return (
     <>
       <Header />
-      <BudgetForm />
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
+      </div>
     </>
   );
 };
