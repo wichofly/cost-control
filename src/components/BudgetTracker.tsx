18be88a6008a2fe07cb173e93f16d876a4fb3d33
Mauricio Erazo <antonierazo13@hotmail.com>
2024-12-21 20:21:05 +0100

    feat: Add circular progress bar to BudgetTracker for visualizing budget expenditure

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,13 +1,37 @@
 import { useBudget } from '../hooks/useBudget';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { AmountDisplay } from './AmountDisplay';
 
 export const BudgetTracker = () => {
   const { state, totalExpenses, remainingBudget } = useBudget();
 
+  const percentage = Number(((totalExpenses / state.budget) * 100).toFixed(2)); // const percentage = +((totalExpenses / state.budget) * 100).toFixed(2);
+  console.log(percentage);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
-        <img src="/graph.jpg" alt="Expense graph" />
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({
+            pathColor:
+              percentage === 100
+                ? '#DC2626'
+                : percentage >= 75
+                ? '#FF8C00'
+                : '#00BFFF',
+            trailColor: '#D1D5DB',
+            textSize: 10,
+            textColor:
+              percentage === 100
+                ? '#DC2626'
+                : percentage >= 75
+                ? '#FF8C00'
+                : '#00BFFF',
+          })}
+          text={`${percentage}% Expended`}
+        />
       </div>
 
       <div className="flex flex-col justify-center items-center gap-8">
