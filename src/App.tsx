00d87266ca3ed8075d4b55b8fdb04475e7088aa3
Mauricio Erazo <antonierazo13@hotmail.com>
2025-01-07 17:45:04 +0100

    fix: comeback not using the pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useMemo } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { BudgetForm } from './components/BudgetForm';
 import { Header } from './components/Header';
 import { useBudget } from './hooks/useBudget';
@@ -7,9 +6,6 @@ import { BudgetTracker } from './components/BudgetTracker';
 import ExpenseModal from './components/ExpenseModal';
 import { ExpenseList } from './components/ExpenseList';
 import { FilterByCategory } from './components/FilterByCategory';
-import { Register } from './components/Register';
-import { Login } from './components/Login';
-import { PrivateRoute } from './components/PrivateRoute';
 
 const App = () => {
   const { state } = useBudget();
@@ -22,30 +18,19 @@ const App = () => {
   }, [state]);
 
   return (
-    <Router>
+    <>
       <Header />
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-                {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
-              </div>
-              {isValidBudget && (
-                <div className="max-w-3xl mx-auto py-10">
-                  <FilterByCategory />
-                  <ExpenseList />
-                  <ExpenseModal />
-                </div>
-              )}
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </Router>
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
+      </div>
+      {isValidBudget && (
+        <div className="max-w-3xl mx-auto py-10">
+          <FilterByCategory />
+          <ExpenseList />
+          <ExpenseModal />
+        </div>
+      )}
+    </>
   );
 };
 
