9d1d5fb531ffa1fc2e2615816fcddcf61c6d8b53
Mauricio Erazo <antonierazo13@hotmail.com>
2025-01-20 00:27:10 +0100

    style: improve styling and layout of NotFound page for better user experience

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <main className="text-center py-10">
+    <main className="text-center py-10 p-4">
       <h1 className="text-4xl font-bold text-red-600">404 - Page Not Found</h1>
-      <p>The page you're looking for does not exist.</p>
+      <p className="mt-4 text-gray-600 text-xl font-semibold">
+        The page you're looking for does not exist.
+      </p>
       <Link
         to="/"
-        className="bg-blue-600 text-white px-4 py-2 rounded mt-4 inline-block"
+        className="bg-blue-600 text-white px-4 py-2 rounded-md mt-4 inline-block"
       >
         Go Back Home
       </Link>
